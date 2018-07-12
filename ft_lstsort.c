64bf019d81522862776363de94ebd4fbf1659f5f
Poitier Stringer <pstringe@e1z3r3p18.42.us.org>
2018-07-12 13:34:14 -0700

    removed ft_printf submodule

--- ft_lstsort.c
+++ ft_lstsort.c
@@ -6,33 +6,23 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/12 10:09:13 by pstringe          #+#    #+#             */
-/*   Updated: 2018/07/12 10:41:53 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/07/12 12:48:10 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int		default_cmp_function(void *ptr1, void *ptr2)
-{
-	char		*str1;
-	char		*str2;
-
-	str1 = (char*)ptr1;
-	str2 = (char*)ptr2;
-	return (ft_strcmp(str1, str2));
-}
 
 static void		swap_links(t_list *l1, t_list *l2)
 {
 	void		*tmp_data;
 	size_t		tmp_size;
 
-	tmp_data = l1->data;
-	tmp_size = l1->size;
-	l1->data = l2->data;
-	l1->size = l2->size;
-	l2->data = tmp_data;
-	l2->size = tmp_size;
+	tmp_data = l1->content;
+	tmp_size = l1->content_size;
+	l1->content = l2->content;
+	l1->content_size = l2->content_size;
+	l2->content = tmp_data;
+	l2->content_size = tmp_size;
 }
 
 void			ft_lstsort(t_list *begin, int (*cmp)(void *, void *, void **, int), void **aux, int len)
@@ -42,8 +32,6 @@ void			ft_lstsort(t_list *begin, int (*cmp)(void *, void *, void **, int), void 
 
 	if (begin == NULL)
 		return ;
-	if (cmp == NULL)
-		cmp = default_cmp_function;
 	sorted = 0;
 	while (!sorted)
 	{
@@ -51,7 +39,7 @@ void			ft_lstsort(t_list *begin, int (*cmp)(void *, void *, void **, int), void 
 		link = begin;
 		while (link->next)
 		{
-			if (cmp(link->data, link->next->data, aux, len) > 0)
+			if (cmp(link->content, link->next->content, aux, len) > 0)
 			{
 				swap_links(link, link->next);
 				sorted = 0;
